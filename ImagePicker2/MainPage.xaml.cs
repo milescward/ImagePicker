59bd5d864ee121c9f295e264fea779679245201a
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-11-12 20:35:39 -0800

    First commit

---
+++ ImagePicker2/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImagePicker2.Services;
+using Xamarin.Forms;
+
+
+namespace ImagePicker2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnPickPhotoButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).IsEnabled = false;
+
+            Stream stream = await DependencyService.Get<IPhotoPickerService>().GetImageStreamAsync();
+            if (stream != null)
+            {
+
+                image.Source = ImageSource.FromStream(() => stream);
+            }
+
+            (sender as Button).IsEnabled = true;
+        }
+    }
+}
---
+++ ImagePicker2/Services/IPhotoPickerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImagePicker2.Services
+{
+    public interface IPhotoPickerService
+    {
+        Task<Stream> GetImageStreamAsync();
+    }
+}
